7f993a4bf05159cdaf90142fb274ddc2b4a177e0
Alaa El-Ebshihy <alaa.el-ebshihy@researchstudio.at>
2022-08-11 13:10:29 +0200

    Added the basic interface for delta calculation.

---
+++ code/delta/delta_calculation_interface.py
@@ -0,0 +1,10 @@
+class DocumentDeltaCalculationInterface:
+
+    def calculate_document_delta_score(self, input_df_path, output_path):
+        """
+        a function to calculate a numerical score between pairs of document in clusters
+        :param input_df_path: the path of the data frame that stores documents and clusters information
+        :param output_path: the output path for the calculated pairs scores
+        :return:
+        """
+        pass
