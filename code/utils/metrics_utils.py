061d3154c36b3a7575fe01a8b8f56ecdf942d1bb
Alaa El-Ebshihy <alaa.el-ebshihy@researchstudio.at>
2022-08-23 15:36:02 +0200

    Added person correlation to similarity metrics.

--- code/utils/metrics_utils.py
+++ code/utils/metrics_utils.py
@@ -2,9 +2,12 @@
 A script contains different functions to do some calculations for metrics
 """
 from scipy import spatial
+from scipy import stats
 
 
 def calculate_similarity_between_vectors(metric_key, vector1, vector2):
     if metric_key == 'cosine':
         return 1 - spatial.distance.cosine(vector1, vector2)
+    if metric_key == 'pearson':
+        return stats.pearsonr(vector1, vector2)[0]
     return 0
