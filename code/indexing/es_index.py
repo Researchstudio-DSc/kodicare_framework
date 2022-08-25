375f78196e0e1092193d34aab5660a6c5fedc896
Alaa El-Ebshihy <alaa.el-ebshihy@researchstudio.at>
2022-08-25 16:29:26 +0200

    Added the query id to the request of the ranking.

--- code/indexing/es_index.py
+++ code/indexing/es_index.py
@@ -3,11 +3,16 @@ from elasticsearch.client import IndicesClient
 import json
 import requests
 
+MAP_KEY__QUERY_TEXT = "query_text"
+MAP_KEY__QUERY_ID = "query_id"
+MAP_KEY__RELEVANT_DOCS = "relevant_docs"
+
+
 # simple query template
 def build_bool_query(query, size):
     data = {
-        "from" : 0, 
-        "size" : size,
+        "from": 0,
+        "size": size,
         "query": {
             "bool": {
                 "should": {
@@ -24,10 +29,10 @@ def build_bool_query(query, size):
 # simple query template
 def build_multimatch_query(query, fields, size):
     data = {
-        "from" : 0, 
-        "size" : size,
+        "from": 0,
+        "size": size,
         "query": {
-            "multi_match" : {
+            "multi_match": {
                 "query": query,
                 "type": "cross_fields",
                 "fields": fields
@@ -38,14 +43,13 @@ def build_multimatch_query(query, fields, size):
 
 
 class Index:
-    
+
     def __init__(self, index_name, host="localhost:9200", es=None, ic=None):
         self.index_name = index_name
         self.host = host
         self.es = es if es != None else Elasticsearch(hosts=[host])
         self.ic = ic if ic != None else IndicesClient(self.es)
-    
-    
+
     def create_index(self, index_body):
         """
         Create the index, recreate it if it exists
@@ -53,16 +57,14 @@ class Index:
         if self.ic.exists(index=self.index_name):
             self.ic.delete(index=self.index_name)
         self.ic.create(index=self.index_name, body=index_body)
-    
-    
+
     def delete_index(self):
         """
         Delete the index
         """
         if self.ic.exists(index=self.index_name):
             self.ic.delete(index=self.index_name)
-        
-    
+
     def update_settings(self, index_body):
         """
         Update settings of the index.
@@ -76,14 +78,12 @@ class Index:
         self.ic.close(index=self.index_name)
         self.ic.put_settings(settings=settings, index=self.index_name)
         self.ic.open(index=self.index_name)
-        
-    
+
     def index_exists(self):
         """
         Return True if index exists
         """
         return self.ic.exists(self.index_name)
-    
 
     def action_gen(self, doc_data):
         for passage_body in doc_data:
@@ -91,7 +91,7 @@ class Index:
                 '_index': self.index_name,
                 '_source': passage_body
             }
-    
+
     def index_docs(self, doc_iterator):
         """
         Index documents via iterator using the bulk function
@@ -99,7 +99,7 @@ class Index:
         for batch_data in doc_iterator:
             data_json = ""
             for doc_id, doc_source in batch_data:
-                action_and_meta_data = { "index" : { "_index" : self.index_name, "_id" : doc_id} }
+                action_and_meta_data = {"index": {"_index": self.index_name, "_id": doc_id}}
                 data_json += json.dumps(action_and_meta_data) + "\n"
                 data_json += json.dumps(doc_source) + "\n"
             headers = {
@@ -107,7 +107,6 @@ class Index:
             }
             r = requests.post(f"http://{self.host}/_bulk", data=data_json, headers=headers)
             resp = r.json()
-    
 
     def rank(self, queries: list, size=100, query_builder=None):
         """
@@ -117,7 +116,7 @@ class Index:
         data_json = ""
         for query in queries:
             q_header = {}
-            query_data = query_builder.build(query, size=size)
+            query_data = query_builder.build(query[MAP_KEY__QUERY_TEXT], size=size)
             data_json += json.dumps(q_header) + "\n"
             data_json += json.dumps(query_data) + "\n"
         headers = {
@@ -126,8 +125,14 @@ class Index:
         r = requests.post(f"http://{self.host}/{self.index_name}/_msearch", data=data_json, headers=headers)
         responses = r.json()['responses']
         ranking_data = []
-        for ranking in responses:
+        for index, ranking in enumerate(responses):
             if "hits" not in ranking:
                 print(ranking)
-            ranking_data.append([hit['_source'] for hit in ranking["hits"]["hits"]])
+            query_info = queries[index]
+            ranking_data.append({
+                MAP_KEY__QUERY_TEXT: query_info[MAP_KEY__QUERY_TEXT],
+                MAP_KEY__QUERY_ID: query_info[MAP_KEY__QUERY_ID],
+                MAP_KEY__RELEVANT_DOCS:
+                    [{"score": hit['_score'], "info": hit['_source']} for hit in ranking["hits"]["hits"]]
+            })
         return ranking_data
