8c1a8ca7d7dccbb8e07287668d24a424dc3d5d8a
Tobias Fink <tobiasfink-ai@gmail.com>
2022-09-08 14:57:08 +0200

    Doc and paragraph index types now calculate document scores differently

--- code/evaluation/eval.py
+++ code/evaluation/eval.py
@@ -6,6 +6,32 @@ from lxml import etree
 from code.indexing.es_index import Index
 from code.indexing.readers import CORD19Reader, CORD19ParagraphReader
 
+def doc_reranking(cord_ranking):
+    # rerank the documents using the original ranking
+    # but filter out duplicates
+    found_ids = set()
+    reranking = []
+    for cord_uid, entry in cord_ranking:
+        if cord_uid in found_ids:
+            continue
+        found_ids.add(cord_uid)
+        reranking.append((cord_uid, entry['_score']))
+    return reranking
+
+
+def reciprocal_rank_score(rank):
+    return 1/(10+rank)
+
+def paragraph_reranking(cord_ranking):
+    # rerank by calculating a score based on the rank positions of a
+    # document's paragraphs in the original ranking
+    cord_uid_scores = {}
+    for rank, (cord_uid, entry) in enumerate(cord_ranking):
+        if cord_uid not in cord_uid_scores:
+            cord_uid_scores[cord_uid] = 0
+        cord_uid_scores[cord_uid] += reciprocal_rank_score(rank)
+    return sorted(cord_uid_scores.items(), key=lambda x: x[1], reverse=True)
+
 
 def main(args):
 
@@ -35,21 +61,26 @@ def main(args):
         reader = CORD19Reader(batch_size=1024)
     index = Index(args.index_name, host=args.host)
 
-    ranking_data = index.rank(queries=queries, size=100, query_builder=reader)
+    ranking_data = index.rank(queries=queries, size=args.size, query_builder=reader)
 
 
     for q_id, raw_ranking in enumerate(ranking_data):
-        found_ids = set()
         rank = 0
+        cord_ranking = []
+        # get general mapping to cord ids
         for entry in raw_ranking:
-            score = entry['_score']
+            #score = entry['_score']
             paper_id = entry['_source']['doc_id']
             cord_uid = cord_uid_mapping[paper_id]
-            if cord_uid in found_ids:
-                continue
-            found_ids.add(cord_uid)
-            print(f"{q_id+1} Q0 {cord_uid} {rank} {score} {args.run_name}")
-            rank += 1
+            cord_ranking.append((cord_uid, entry))
+        
+        if args.index_type == "paragraphs":
+            reranking = paragraph_reranking(cord_ranking)
+        else: # whole documents
+            reranking = doc_reranking(cord_ranking)
+        
+        for rank, (cord_uid, score) in enumerate(reranking):
+            print(f"{q_id+1} Q0 {cord_uid} {rank} {score:.4f} {args.run_name}")
     
     #with open(args.run_settings, "r") as fp:
     #    run_settings = json.load(fp)
@@ -67,6 +98,7 @@ if __name__ == "__main__":
     parser.add_argument('--index_name', help='The name of the index')
     parser.add_argument('--index_type', help='paragraphs or doc')
     parser.add_argument('--run_name', help='Name of the run')
+    parser.add_argument('--size', type=int, default=100, help='Number of results to retrieve')
     #parser.add_argument("--run_settings", help='File with the run settings')
     args = parser.parse_args()
     main(args)
