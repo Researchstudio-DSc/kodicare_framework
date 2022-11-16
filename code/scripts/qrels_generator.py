5f1c95df78b165911eea5c7d84794d83ae0a77e4
Alaa El-Ebshihy <alaa.el-ebshihy@researchstudio.at>
2022-11-16 10:47:50 +0100

    Generalized the qrels generator.

--- code/scripts/qrels_generator.py
+++ code/scripts/qrels_generator.py
@@ -7,7 +7,7 @@ import hydra
 from code.utils import io_util
 
 
-def write_to_qrels(run_name, run_result_path, qrels_out_path):
+def write_to_qrels(run_name, qrel_id_field, run_result_path, qrels_out_path):
     run_data = io_util.read_json(run_result_path)
     qrels_list = []
 
@@ -15,9 +15,9 @@ def write_to_qrels(run_name, run_result_path, qrels_out_path):
         query_id = query['query_id']
         rank = 0
         for doc in query['relevant_docs']:
-            cord_id = doc['cord_uid']
-            print(f"{query_id} Q0 {cord_id} {rank} {doc['score']:.4f} {run_name}")
-            qrels_list.append(f"{query_id} Q0 {cord_id} {rank} {doc['score']:.4f} {run_name}")
+            qrel_id = doc[qrel_id_field]
+            print(f"{query_id} Q0 {qrel_id} {rank} {doc['score']:.4f} {run_name}")
+            qrels_list.append(f"{query_id} Q0 {qrel_id} {rank} {doc['score']:.4f} {run_name}")
             rank += 1
     io_util.write_list_to_file(qrels_out_path, qrels_list)
 
@@ -27,12 +27,12 @@ def main(cfg):
     run_result_path = io_util.join(cfg.config.root_dir, io_util.join(cfg.config.output_dir,
                                                                      cfg.retrieval.rerank_path))
     run_name = cfg.retrieval.run_name
+    qrel_id_field = cfg.retrieval.qrel_id_field
     qrels_out_path = io_util.join(cfg.config.root_dir, io_util.join(cfg.config.output_dir,
                                                                     cfg.retrieval.run_name + '.txt'))
 
-    write_to_qrels(run_name, run_result_path, qrels_out_path)
+    write_to_qrels(run_name, qrel_id_field, run_result_path, qrels_out_path)
 
 
 if __name__ == '__main__':
     main()
-
