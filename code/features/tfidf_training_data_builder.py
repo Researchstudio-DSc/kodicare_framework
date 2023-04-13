7de1c5f9f29a022ab2d5a190f1a13386815b52e1
Alaa El-Ebshihy <alaa.el-ebshihy@researchstudio.at>
2023-04-13 15:46:15 +0200

    Added training data builder interface and tfidf implementation.

---
+++ code/features/tfidf_training_data_builder.py
@@ -0,0 +1,125 @@
+"""
+implement training data builder for the tfidf representation
+"""
+
+import numpy as np
+import pandas as pd
+from numpy import dot
+from numpy.linalg import norm
+
+from code.data import evolving_dtc_splits_parser
+from code.features import training_data_builder_interface
+from code.utils import io_util
+
+START_STEP = 2
+END_STEP = 10
+
+SYSTEMS = ['bm25_qe_run', 'bm25_run', 'dirLM_qe_run', 'dirLM_run', 'dlh_qe_run', 'dlh_run', 'pl2_qe_run', 'pl2_run']
+METRICS = ['P_10', 'Rprec', 'bpref', 'map', 'ndcg', 'ndcg_cut_10', 'recip_rank']
+
+
+def construct_tc_rep(vocab_dict, doc_vectors):
+    tc_rep = np.zeros((len(doc_vectors), len(vocab_dict)))
+    for doc_ind, doc_vec in enumerate(doc_vectors):
+        for k, v in doc_vec:
+            tc_rep[doc_ind][k] = v
+
+    return tc_rep
+
+
+def vec_diff(a, b):
+    cos_sim = dot(a, b) / (norm(a) * norm(b))
+    return cos_sim
+
+
+def init_rd_map():
+    res_change_map = {}
+    for system in SYSTEMS:
+        for metric in METRICS:
+            res_change_map[system + '-' + metric] = []
+    return res_change_map
+
+
+def update_res_change_map(res_change_map, evolving_dtc_parser, interval_start1, interval_end1, interval_start2,
+                          interval_end2):
+    for system in SYSTEMS:
+        for metric in METRICS:
+            results = evolving_dtc_parser.get_avg_run_evaluation(system, 'dtc_evolving_eval', metric)
+            res_interval1 = results[interval_start1:interval_end1]
+            res_interval2 = results[interval_start2:interval_end2]
+            res_change_map[system + '-' + metric].append(
+                evolving_dtc_parser.is_significant_change(res_interval1, res_interval2))
+
+
+class TFIDFTrainingDataBuilder(training_data_builder_interface.TrainingDataBuilderInterface):
+    def __init__(self, dtc_dir, evaluation_file, representation_dir, epochs=41):
+        self.dtc_dir = dtc_dir
+        self.evaluation_file = evaluation_file
+        self.representation_dir = representation_dir
+        self.epochs = epochs
+
+    def build_kd_feature_vector(self):
+        vocab_dict = io_util.read_pickle(io_util.join(self.dtc_dir, self.representation_dir + "/vocab.pkl"))
+        diff_vectors = []
+        tc_names = []
+
+        for step in range(START_STEP, END_STEP):
+            for tc_ind in range(0, self.epochs - step):
+                for tc_ind2 in range(tc_ind + step, self.epochs - step):
+                    print('kd for', tc_ind, tc_ind2, step)
+                    if tc_ind2 >= self.epochs:
+                        break
+                    vec1 = io_util.read_pickle(
+                        io_util.join(self.dtc_dir, self.representation_dir + '/' + str(tc_ind) + '_tfidf_full_vec.pkl'))
+                    vec2 = io_util.read_pickle(io_util.join(self.dtc_dir, self.representation_dir + '/' + str(
+                        tc_ind2) + '_tfidf_full_vec.pkl'))
+                    tc_rep1 = construct_tc_rep(vocab_dict, vec1)
+                    tc_rep2 = construct_tc_rep(vocab_dict, vec2)
+                    # get the difference between representation of the same word between two different test collections
+                    voc_len = len(vocab_dict)
+                    diag_vec = np.zeros(voc_len)
+                    for voc_ind in range(voc_len):
+                        vec_length = min(len(tc_rep1[:, voc_ind]), len(tc_rep2[:, voc_ind]))
+                        diag_vec[voc_ind] = vec_diff(tc_rep1[:, voc_ind][:vec_length], tc_rep2[:, voc_ind][:vec_length])
+                    diff_vectors.append(diag_vec)
+                    tc_names.append(
+                        str(tc_ind) + '-' + str(tc_ind + step) + ':' + str(tc_ind2) + '-' + str(tc_ind2 + step))
+
+        features_df = pd.DataFrame(diff_vectors, columns=[vocab_dict[key] for key in vocab_dict.keys()])
+        features_df['tc_name'] = tc_names
+
+        # add some calculations
+        kd_norm = np.zeros(len(tc_names))
+        kd_min = np.zeros(len(tc_names))
+        kd_max = np.zeros(len(tc_names))
+        kd_avg = np.zeros(len(tc_names))
+
+        for i in range(len(tc_names)):
+            data = diff_vectors[i]
+            new_data = data[~np.isnan(data)]
+            kd_norm[i] = norm(new_data)
+            kd_min[i] = np.min(new_data)
+            kd_max[i] = np.max(new_data)
+            kd_avg[i] = np.average(new_data)
+        features_df['kd_norm'] = kd_norm
+        features_df['kd_min'] = kd_min
+        features_df['kd_max'] = kd_max
+        features_df['kd_avg'] = kd_avg
+
+        return features_df
+
+    def build_rd_df(self):
+        res_change_map = init_rd_map()
+        evaluation_splits_path = io_util.join(self.dtc_dir, self.evaluation_file)
+        evolving_dtc_parser = evolving_dtc_splits_parser.EvolvingDTCSplitsParser(evaluation_splits_path)
+
+        for step in range(START_STEP, END_STEP):
+            for tc_ind in range(0, self.epochs - step):
+                for tc_ind2 in range(tc_ind + step, self.epochs - step):
+                    print('RD for', tc_ind, tc_ind2, step)
+                    if tc_ind2 >= self.epochs:
+                        break
+                    update_res_change_map(res_change_map, evolving_dtc_parser, tc_ind, tc_ind + step, tc_ind2,
+                                          tc_ind2 + step)
+
+        return pd.DataFrame.from_dict(res_change_map)
