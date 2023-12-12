648ddc1b0337643c6b57f10f140bd4cb9ff535c2
Alaa El-Ebshihy <alaa.el-ebshihy@researchstudio.at>
2023-12-12 14:46:56 +0100

    Changed RD prediction to take the csv files instead of the evaluation

--- code/result_delta/binary_rd_labelling.py
+++ code/result_delta/binary_rd_labelling.py
@@ -5,57 +5,22 @@ Implement result delta labels using binary labels
 import pandas as pd
 
 from code.data import evolving_dtc_splits_parser
-from code.result_delta import result_delta_labeling_interface
-from code.utils import io_util
-
-SYSTEMS = ['bm25_qe_run', 'bm25_run', 'dirLM_qe_run', 'dirLM_run', 'dlh_qe_run', 'dlh_run', 'pl2_qe_run', 'pl2_run']
-METRICS = ['P_10', 'Rprec', 'bpref', 'map', 'ndcg', 'ndcg_cut_10', 'recip_rank']
+from code.result_delta.result_delta_labeling_interface import *
+from code.utils.io_util import *
 
 
-def init_rd_map():
-    res_change_map = {}
-    for system in SYSTEMS:
-        for metric in METRICS:
-            res_change_map[system + '-' + metric] = []
-    return res_change_map
-
-
-def update_res_change_map(res_change_map, evolving_dtc_parser, interval_start1, interval_end1, interval_start2,
-                          interval_end2, normal_dist_pairs, not_normal_dist_pairs):
-    for system in SYSTEMS:
-        for metric in METRICS:
-            acc_res_1 = []  # will contain results for topics for each test collection in the first interval
-            acc_res_2 = []  # will contain results for topics for each test collection in the second interval
-
-            for i in range(interval_start1, interval_end1):
-                acc_res_1 += evolving_dtc_parser.get_collection_runs_evaluation_list(system, 'dtc_evolving_eval',
-                                                                                     metric, i)
-            for i in range(interval_start2, interval_end2):
-                acc_res_2 += evolving_dtc_parser.get_collection_runs_evaluation_list(system, 'dtc_evolving_eval',
-                                                                                     metric, i)
-            # TODO: add here test for normal distribution
-            if evolving_dtc_splits_parser.is_data_normal(acc_res_1) and evolving_dtc_splits_parser.is_data_normal(
-                    acc_res_2):
-                normal_dist_pairs += 1
-            else:
-                not_normal_dist_pairs += 1
-
-            res_change_map[system + '-' + metric].append(
-                evolving_dtc_splits_parser.is_significant_change(acc_res_1, acc_res_2))
-
-
-class BinaryRDLabelling(result_delta_labeling_interface.ResultDeltaLabelingInterface):
-    def __init__(self, dtc_dir, evaluation_file, epochs, start_step, end_step):
-        self.dtc_dir = dtc_dir
-        self.evaluation_file = evaluation_file
+class BinaryRDLabelling(ResultDeltaLabelingInterface):
+    def __init__(self, evaluation_dir, epochs, start_step, end_step, systems, metrics):
+        self.evaluation_dir = evaluation_dir
         self.epochs = epochs
         self.start_step = start_step
         self.end_step = end_step
+        self.systems = systems
+        self.metrics = metrics
+        self.tc_system_metric_res_maps = self.init_tc_system_metric_res_maps()
 
     def construct_rd_labels(self):
-        res_change_map = init_rd_map()
-        evaluation_splits_path = io_util.join(self.dtc_dir, self.evaluation_file)
-        evolving_dtc_parser = evolving_dtc_splits_parser.EvolvingDTCSplitsParser(evaluation_splits_path)
+        res_change_map = self.init_rd_map()
         normal_dist_pairs = not_normal_dist_pairs = 0
 
         for step in range(self.start_step, self.end_step):
@@ -64,8 +29,8 @@ class BinaryRDLabelling(result_delta_labeling_interface.ResultDeltaLabelingInter
                     print('RD for', tc_ind, tc_ind2, step)
                     if tc_ind2 >= self.epochs:
                         break
-                    update_res_change_map(res_change_map, evolving_dtc_parser, tc_ind, tc_ind + step, tc_ind2,
-                                          tc_ind2 + step, normal_dist_pairs, not_normal_dist_pairs)
+                    self.update_res_change_map(res_change_map, tc_ind, tc_ind + step, tc_ind2,
+                                               tc_ind2 + step, normal_dist_pairs, not_normal_dist_pairs)
 
         rd_df = pd.DataFrame.from_dict(res_change_map)
         rd_df['tc_name'] = self.get_tc_names()
@@ -82,3 +47,55 @@ class BinaryRDLabelling(result_delta_labeling_interface.ResultDeltaLabelingInter
                     tc_names.append(
                         str(tc_ind) + '-' + str(tc_ind + step) + ':' + str(tc_ind2) + '-' + str(tc_ind2 + step))
         return tc_names
+
+    def init_rd_map(self):
+        res_change_map = {}
+        for system in self.systems:
+            for metric in self.metrics:
+                res_change_map[system + '-' + metric] = []
+        return res_change_map
+
+    def init_tc_system_metric_res_maps(self):
+        tc_system_metric_res_maps = []
+        for i in range(self.epochs):
+            tc_system_metric_res_maps.append(self.construct_system_metric_res_map(i))
+        return tc_system_metric_res_maps
+
+    def construct_system_metric_res_map(self, tc_id):
+        results_df = pd.read_csv(join(self.evaluation_dir, 'tc_' + str(tc_id) + '.csv'), sep='\t')
+        system_metric_res_map = self.init_system_metric_res_map()
+        for i in range(len(results_df)):
+            for system in self.systems:
+                system_metric_res_map[system][results_df['metric'][i]].append(results_df[system][i])
+        return system_metric_res_map
+
+    def init_system_metric_res_map(self):
+        system_metric_res_map = {}
+        for system in self.systems:
+            system_metric_res_map[system] = {}
+            for metric in self.metrics:
+                system_metric_res_map[system][metric] = []
+        return system_metric_res_map
+
+    def update_res_change_map(self, res_change_map, interval_start1, interval_end1, interval_start2,
+                              interval_end2, normal_dist_pairs, not_normal_dist_pairs):
+        for system in self.systems:
+            for metric in self.metrics:
+                acc_res_1 = []  # will contain results for topics for each test collection in the first interval
+                acc_res_2 = []  # will contain results for topics for each test collection in the second interval
+
+                for i in range(interval_start1, interval_end1):
+                    acc_res_1 += self.tc_system_metric_res_maps[i][system][metric]
+
+                for i in range(interval_start2, interval_end2):
+                    acc_res_2 += self.tc_system_metric_res_maps[i][system][metric]
+
+                # TODO: add here test for normal distribution
+                if evolving_dtc_splits_parser.is_data_normal(acc_res_1) and evolving_dtc_splits_parser.is_data_normal(
+                        acc_res_2):
+                    normal_dist_pairs += 1
+                else:
+                    not_normal_dist_pairs += 1
+
+                res_change_map[system + '-' + metric].append(
+                    evolving_dtc_splits_parser.is_significant_change(acc_res_1, acc_res_2))
--- code/scripts/result_delta/binary_rd_labelling_executer.py
+++ code/scripts/result_delta/binary_rd_labelling_executer.py
@@ -1,20 +1,21 @@
 import hydra
 
-from code.result_delta import binary_rd_labelling
-from code.utils import io_util
+from code.result_delta.binary_rd_labelling import *
+from code.utils.io_util import *
 
 
 @hydra.main(version_base=None, config_path="../../../conf", config_name=None)
 def main(cfg):
-    dtcs_content_dir = io_util.join(cfg.config.root_dir, cfg.dtc.evaluation_splits_dir)
+    dtcs_content_dir = join(cfg.config.root_dir, cfg.dtc.evaluation_splits_dir)
+    evaluation_perquery_output_path = join(dtcs_content_dir, cfg.dtc.evaluation.evaluation_perquery_output_path)
 
-    evaluation_file = cfg.dtc.evaluation_splits_path
+    systems = [retrieval_model[index]['run_name'] for index, retrieval_model in enumerate(cfg.retrieval_models)]
 
-    binary_rd_instance = binary_rd_labelling.BinaryRDLabelling(dtcs_content_dir, evaluation_file, cfg.config.epochs,
-                                                               cfg.config.start_step, cfg.config.end_step, )
+    binary_rd_instance = BinaryRDLabelling(evaluation_perquery_output_path, cfg.dtc.epochs, cfg.dtc.start_step,
+                                           cfg.dtc.end_step, systems, cfg.evaluation_metrics)
     rd_df = binary_rd_instance.construct_rd_labels()
 
-    rd_df.to_pickle(io_util.join(dtcs_content_dir, cfg.dtc.rd_label_path))
+    rd_df.to_pickle(join(dtcs_content_dir, cfg.dtc.rd_label_path))
 
 
 if __name__ == '__main__':
